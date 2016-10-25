4822783e6f5e0e32a2d142f489ea3f2e52389286
Firas Krichi <firas.krichi@gmail.com>
2016-10-24 19:52:44 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Image
+} from 'react-native';
+
+export default class fivemin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={{flex: 1}}>
+          <Image source={{uri: 'https://source.unsplash.com/user/firaskrichi/likes'}} resizeMode="cover" style={{width: 400, height: 350}} />
+          <View style={styles.day}>
+            <View style={styles.block}>
+              <Text style={styles.text}>
+                I am grateful for ...
+              </Text>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+            </View>
+            <View style={styles.block}>
+
+              <Text style={styles.text}>
+                What will I do to make today great?
+              </Text>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+            </View>
+
+            <View style={styles.block}>
+
+              <Text style={styles.text}>
+                Daily affirmations, I am ...
+              </Text>
+
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+            </View>
+
+          </View>
+          <View style={styles.night}>
+
+            <View style={styles.block}>
+
+              <Text style={styles.text}>
+                3 Amazing things that happened today ...
+              </Text>
+
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+            </View>
+
+            <View style={styles.block}>
+              <Text style={styles.text}>
+                How could I have made today even better?
+              </Text>
+              <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText}/>
+              </View>
+            </View>
+
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 0,
+  },
+  card: {
+    backgroundColor: 'yellow',
+  },
+  text: {
+    textAlign: 'center',
+    margin: 10,
+    color: '#333333',
+    fontSize: 16,
+  },
+  inputContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+  },
+  inputText: {
+    height: 35,
+    fontSize: 14,
+  },
+  day: {
+    backgroundColor: '#FFFEF9',
+    padding: 5,
+  },
+  night: {
+    backgroundColor: '#DFDFD5',
+    padding: 5,
+  },
+  block: {
+    marginBottom: 30,
+  }
+
+});
+
+AppRegistry.registerComponent('fivemin', () => fivemin);
